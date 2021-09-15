b4327e8078cfd54b6079b3001c2817f845ae82f7
richardvdoost <richard@blendergrid.com>
2021-09-15 05:44:34 +0200

    Improve sleep, add fade to volume, add playlists

--- spotify.py
+++ spotify.py
@@ -4,17 +4,60 @@ import argparse
 import subprocess
 import time
 
+PLAYLISTS = {
+    "chill": ("3yHsqOLOzoFEm60M99SWJv", "6tunhVGD8C05MZNjSVIsjw"),
+    "favorites": ("6ni68MkGnCQhyoJohHiuHC", "37BZB0z9T8Xu7U3e65qxFy"),
+    "sfw": ("5Sd3qK4AvwE0nQzcscMHMA", "5W3N9NkOl0tLaqi5LZbUOC"),
+    "old": ("2KGsnNbtbs4oTnVue3Cb4a", "0X3LTPRlsqs7rFZGVy6SSH"),
+    "bedtimes": ("64VdS4dsqof17W3L3gAbE1", "5xKw68A720RT2Q4XrsmMg2"),
+}
+
+MIN_VOLUME = 8
+PLAYLIST_INIT_VOLUME = 40
+PLAYLIST_FADE_IN = 30
+
 
 def main():
+    COMMANDS = {
+        "play": play,
+        "pause": pause,
+        "volume": volume,
+        "fade": fade,
+        "sleep": sleep,
+    }
 
-    COMMANDS = {"volume": volume, "sleep": sleep}
+    choices = list(COMMANDS.keys()) + list(PLAYLISTS.keys())
 
-    parser = argparse.ArgumentParser(description="Spotify Automation - Tools to automate Spotify on Mac")
-    parser.add_argument("command", choices=COMMANDS.keys())
-    parser.add_argument("parameters", type=str, help="Parameters", nargs="+")
+    parser = argparse.ArgumentParser(
+        description="Spotify Automation - Tools to automate Spotify on Mac"
+    )
+    parser.add_argument("command", choices=choices)
+    parser.add_argument("parameters", type=str, help="Parameters", nargs="*")
     args = parser.parse_args()
 
-    COMMANDS[args.command](args.parameters)
+    if args.command in COMMANDS:
+        COMMANDS[args.command](args.parameters)
+    elif args.command in PLAYLISTS:
+        print(f"Start Playlist: {args.command}")
+        start_playlist(*PLAYLISTS[args.command])
+
+
+def start_playlist(playlist, track):
+    set_volume(PLAYLIST_INIT_VOLUME)
+
+    osascript(
+        f'tell application "Spotify" to play track "spotify:track:{track}" in context "spotify:playlist:{playlist}"'
+    )
+
+    fade_volume(100, PLAYLIST_FADE_IN)
+
+
+def play(parameters=None):
+    osascript('tell application "Spotify" to play')
+
+
+def pause(parameters=None):
+    osascript('tell application "Spotify" to pause')
 
 
 def volume(parameters):
@@ -34,21 +77,81 @@ def volume(parameters):
     set_volume(int(command))
 
 
+def fade(parameters):
+    if len(parameters) == 2:
+        target_volume = int(parameters[0])
+        duration = float(parameters[1]) * 60
+
+    elif len(parameters) == 1:
+        target_volume = int(parameters[0])
+        duration = 10
+
+    else:
+        raise Exception("Fade needs two parameters: [target volume] [duration]")
+
+    print(
+        f"Fading Spotify volume from {get_volume()} to {target_volume} in {duration:.0f} seconds"
+    )
+
+    fade_volume(target_volume, duration)
+
+
 def sleep(parameters):
-    print(f"Sleep: {parameters}")
+    print("Spotify Sleep")
 
-    wait = parameters[0] if len(parameters) > 1 else 0
-    time.sleep(int(wait))
+    orig_volume = get_volume()
 
-    current_level = get_volume()
-    pause = float(parameters[1]) / current_level
-    for level in range(current_level - 1, 8, -1):
-        start_time = time.time()
-        set_volume(level)
-        time.sleep(max(0, pause + start_time - time.time()))
+    if len(parameters) == 2:
+        print(
+            f"Waiting {parameters[0]} minutes, then fading down volume in {parameters[1]} minutes."
+        )
+        wait = float(parameters[0]) * 60
+        fade_time = float(parameters[1]) * 60
 
-    osascript('tell application "Spotify" to pause')
+        time.sleep(wait)
+        print("Starting volume fade")
+
+    elif len(parameters) == 1:
+        print(f"Fading down volume in {parameters[0]} minutes.")
+        fade_time = float(parameters[0]) * 60
+
+    else:
+        raise Exception(f"Sleep needs one or two parameters, got {len(parameters)}")
+
+    fade_volume(8, fade_time)
+
+    pause()
+    set_volume(orig_volume)
+
     print("Done")
+
+
+def fade_volume(target_volume, duration):
+    start_volume = get_volume()
+    volume_delta = int(abs(start_volume - target_volume))
+
+    if volume_delta == 0:
+        return
+
+    start_time = time.time()
+    elapsed = 0
+    current_volume = start_volume
+    while elapsed < duration:
+        elapsed = time.time() - start_time
+        progress = elapsed / duration
+
+        level = round((1.0 - progress) * start_volume + progress * target_volume)
+        if level == current_volume:
+            time.sleep(0.05)
+            continue
+
+        # print(f"Elapsed: {elapsed:.02f}s - Level: {level}")
+
+        set_volume(level)
+
+        current_volume = level
+        if current_volume == target_volume:
+            break
 
 
 def change_volume(delta):
@@ -65,7 +168,9 @@ def set_volume(level):
 
 
 def osascript(command):
-    completed = subprocess.run(["osascript", "-e", command], check=True, capture_output=True)
+    completed = subprocess.run(
+        ["osascript", "-e", command], check=True, capture_output=True
+    )
     return completed.stdout.decode().strip()
 
 
